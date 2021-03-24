4249fb824154c8d132bca3b99005d0668370a3e5
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-24 18:26:11 -0500

    deleted an empty container and buttons

--- src/javascripts/components/cards/showJokeSetup.js
+++ src/javascripts/components/cards/showJokeSetup.js
@@ -4,8 +4,10 @@ const showJokeSetup = (joke) => {
   document.querySelector('#main-container').innerHTML = `<div class="card" style="width: 30rem; background-color: #686868; height: 35em;">
   <img class="card-img-top mt-3" src="${jokeTitle}" alt="joke generator logo">
   <div class="card-body d-flex flex-column justify-content-center">
-    <h5 class="card-title text-light" id="joke-setup">${joke.setup}</h5>
-    <h5 class="card-title text-light" id="joke-punchline" style="display: none;">${joke.punchline}</h5>
+    <div style="padding-bottom: 4em">
+      <h5 class="card-title text-light" id="joke-setup">${joke.setup}</h5>
+      <h5 class="card-title text-light" id="joke-punchline" style="display: none;">${joke.punchline}</h5>
+    </div>
     <a href="#" id="get-punchline-btn" class="btn bg-dark text-light" style="background-color: black; width: 15em; margin: 0 auto;">GET PUNCHLINE</a>
     <a href="#" id="get-new-joke-btn" class="btn bg-dark text-light" style="background-color: black; width: 15em; margin: 0 auto; display: none;">GET NEW JOKE</a>
   </div>
